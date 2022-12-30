3511f89555e8f645fcb49e6ca32f80e23004bb37
LinggarRN <121592682+LinggarRN@users.noreply.github.com>
2022-12-30 10:43:33 +0700

    Update latihan.php

--- latihan.php
+++ latihan.php
@@ -1,8 +1,8 @@
 <?php 
-echo <h1> created by : LinggarRN <h1>
-echo Data Diri.. <br>
-echo Nama Lengkap : Linggar Rama Nanda <br>
-echo Alamat : Kenteng, Ngadirojo, Wonogiri <br>
-echo Jenis Kelamin : Laki-laki <br>
-echo Warga Negara : WNI <br>
+echo "<h1> created by : LinggarRN <h1>";
+echo "Data Diri.. <br>";
+echo "Nama Lengkap : Linggar Rama Nanda <br>";
+echo "Alamat : Kenteng, Ngadirojo, Wonogiri <br>";
+echo "Jenis Kelamin : Laki-laki <br>";
+echo "Warga Negara : WNI <br>";
 		?>
