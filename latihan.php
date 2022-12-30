387160789701ffa836caaa9cf6975fd1882e716f
LinggarRN <121592682+LinggarRN@users.noreply.github.com>
2022-12-30 10:18:59 +0700

    Data Diri.php
    Data Diri..

---
+++ latihan.php
@@ -0,0 +1,7 @@
+<?php created by : LinggarRN
+      Data Diri..
+      Nama Lengkap : Linggar Rama Nanda
+      Alamat : Kenteng, Ngadirojo, Wonogiri
+      Jenis Kelamin : Laki-laki
+      Warga Negara : WNI
+		?>
